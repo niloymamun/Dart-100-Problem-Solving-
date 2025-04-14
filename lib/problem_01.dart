c9f9e587f4491cfd1aca3485e5797bd4e2e8080b
neloy-mamun <neloymamun06@gmail.com>
2025-04-15 01:18:13 +0600

    add problem solving 01

--- lib/problem_01.dart
+++ lib/problem_01.dart
@@ -1,1 +1,16 @@
+/*
+Problem Solving Question
+Create a program that asks the user to enter their name and their age.
+ Print out a message that tells how many years they have to be 100 years old.
+*/
 
+import 'dart:io';
+
+void main() {
+  stdout.write('What is your name?');
+  String name = stdin.readLineSync()!;
+  print('Hi, $name! what is your age?');
+  int? age = int.tryParse(stdin.readLineSync()!);
+  int yearsToHundred = 100 - age!;
+  print('$name, You Have $yearsToHundred years to be 100');
+}
